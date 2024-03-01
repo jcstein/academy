36aae1dd8d6da2c4a11ac3727c3ee91bfa99bfda
jcstein <mail@jcstein.dev>
2024-02-29 20:23:32 -0500

    feat: rm mware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,6 @@
-import { NextResponse } from 'next/server';
+export { auth as middleware } from "auth"
 
-export function middleware() {
-    // Placeholder: If you don't need middleware logic, you can remove this file entirely
-    return NextResponse.next(); 
-}
-
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"], 
-};
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
