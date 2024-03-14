5c7fb77442e8743f03b3c97b1fdb55b32d9d5e21
jcstein <mail@jcstein.dev>
2024-03-13 20:01:45 -0400

    fix: resolve #1

--- app/modules/1/bubs/page.tsx
+++ app/modules/1/bubs/page.tsx
@@ -1,14 +1,20 @@
 "use client"
-import { useSession, getSession } from "next-auth/react"
-import CustomLink from "@/components/custom-link"
-import { useEffect } from "react"
+import { useState, useEffect } from "react";
+import { useSession, getSession } from "next-auth/react";
+import CustomLink from "@/components/custom-link";
 
 export default function Page() {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
+  // Assume the user is not authenticated initially
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const checkSession = async () => {
       const currentSession = await getSession();
+      // If there's a session, set isAuthenticated to true
+      if (currentSession) {
+        setIsAuthenticated(true);
+      }
       console.log(currentSession);
       if (!currentSession) {
         console.log('No active session found.');
@@ -18,17 +24,19 @@ export default function Page() {
     checkSession();
   }, []);
 
-  useEffect(() => {
-    console.log(`Session status: ${status}`);
-  }, [status]);
-
+  // If the status is 'loading', show a loading message or similar
   if (status === "loading") {
     return <div>Checking if you're signed in 😎...</div>;
   }
-  
-  if (status === "authenticated") {
-    return (
-      <div className="space-y-2">
+
+  // If not authenticated, show the sign-in link
+  if (!isAuthenticated) {
+    return <CustomLink href="/api/auth/signin" className="underline">Sign in with GitHub to get started</CustomLink>;
+  }
+
+  // Content for authenticated users
+  return (
+    <div className="space-y-2">
         <h1 className="text-4xl font-bold pb-4">Getting set up with Bubs</h1>
         <h1 className="text-3xl pb-2">Install MetaMask</h1>
         <p className="pb-4">First, install <CustomLink href="https://metamask.io/download">download MetaMask</CustomLink> if you don't have it already.</p>
@@ -56,11 +64,8 @@ export default function Page() {
         <CustomLink href="/modules/1/writing-your-first-contract" className="px-4 py-2 bg-brand text-white font-bold rounded hover:bg-brand-dark transition duration-300 ease-in-out">
           Next page
         </CustomLink>
-        {/* <div className="pb-4"></div>
-        <p className="border border-gray-200 p-2.5 my-2.5 bg-gray-100 rounded-md">Signed in as {session?.user?.name} ({session?.user?.email})</p> */}
-      </div>
-    )
-  }
-
-  return <CustomLink href="/api/auth/signin" className="underline">Sign in with GitHub to get started</CustomLink>
+        <div className="pb-4"></div>
+        <p className="border border-gray-200 p-2.5 my-2.5 bg-gray-100 rounded-md">Signed in as {session?.user?.name} ({session?.user?.email})</p>
+    </div>
+  );
 }
