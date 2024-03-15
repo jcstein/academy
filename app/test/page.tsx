a06393c798038053a0de4123c214da2d096aa8b6
jcstein <mail@jcstein.dev>
2024-03-15 11:13:41 -0400

    feat: fix markdown renderer styling resolves #3

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,37 @@
 "use client"
 import MarkdownRenderer from '@/components/MarkdownRenderer';
+import { useState, useEffect } from "react";
+import { getSession } from "next-auth/react";
 
-export default function SamplePage() {
+export default function Page() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authCheckCompleted, setAuthCheckCompleted] = useState(false);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const currentSession = await getSession();
+      if (currentSession) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setAuthCheckCompleted(true);
+    };
+
+    checkSession();
+  }, []);
+
+  if (!authCheckCompleted) {
+    return <div>Loading and verifying authentication...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <div>Sign in to continue.</div>;
+  }
+
   return (
-    <div>
+    <div className="space-y-2">
       <MarkdownRenderer filePath="/test/test.md" />
     </div>
   );
-};
+}
