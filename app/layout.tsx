81690fe1cfe84cf53f6226b9bccfe1c26db1683a
jcstein <mail@jcstein.dev>
2024-03-15 15:31:26 -0400

    feat: add plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import Footer from "@/components/footer"
 import Header from "@/components/header"
 import { SessionProvider } from "next-auth/react"
+import PlausibleProvider from 'next-plausible';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,18 +20,20 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children, session = {} }: RootLayoutProps) {
   return (
-    <SessionProvider session={session}>
-      <html lang="en">
-        <body className={inter.className}>
-          <div className="flex flex-col justify-between w-full h-full min-h-screen">
-            <Header />
-            <main className="flex-auto w-full max-w-3xl px-4 py-4 mx-auto sm:px-6 md:py-6">
-              {children}
-            </main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </SessionProvider>
+    <PlausibleProvider domain="modularacademy.vercel.app">
+      <SessionProvider session={session}>
+        <html lang="en">
+          <body className={inter.className}>
+            <div className="flex flex-col justify-between w-full h-full min-h-screen">
+              <Header />
+              <main className="flex-auto w-full max-w-3xl px-4 py-4 mx-auto sm:px-6 md:py-6">
+                {children}
+              </main>
+              <Footer />
+            </div>
+          </body>
+        </html>
+      </SessionProvider>
+    </PlausibleProvider>
   )
 }
